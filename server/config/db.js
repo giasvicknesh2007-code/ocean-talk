b0ffcc2275c955aa2cd6d9f62707912d02701213
giasvicknesh2007-code <giasvicknesh2007@gmail.com>
2026-05-10 00:14:51 +0530

    Fix crypto error by enforcing Node 20 and cleaning DB config

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/oceantalk");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
-        // Don't exit process in development if we want to fallback or just see the error
-        // process.exit(1); 
+        process.exit(1);
     }
 };
 
